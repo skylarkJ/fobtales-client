8003b860146dc1e3d22da3df117d8fc14b829237
skylarkJ <janaryndin@gmail.com>
2017-02-20 19:45:48 -0500

    Change a small style issue and made active a heroku in the config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,9 +2,8 @@
 
 const config = {
   apiOrigins: {
-    development: 'http://localhost:4741'
-    // production: 'https://git.heroku.com/sleepy-hollows-32453.herokuapp.com',
-
+    development: 'http://localhost:4741',
+    production: 'https://git.heroku.com/sleepy-hollows-32453.herokuapp.com'
   },
 };
 
